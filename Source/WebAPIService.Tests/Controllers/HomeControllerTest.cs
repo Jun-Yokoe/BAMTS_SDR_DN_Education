b079bd3973be59914d65f3ed69a37e0f0af85844
Jun.Yokoe <yokoe@bamts.co.jp>
2022-05-18 19:05:29 +0900

    WebAPIService追加直後

---
+++ Source/WebAPIService.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using WebAPIService;
+using WebAPIService.Controllers;
+
+namespace WebAPIService.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // 準備
+            HomeController controller = new HomeController();
+
+            // 実行
+            ViewResult result = controller.Index() as ViewResult;
+
+            // アサート
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
